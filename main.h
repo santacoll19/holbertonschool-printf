8013c746ce7cdfa57a4c9d5e5e35d923d01a79d8
santacoll19 <collazokel19@gmail.com>
2023-07-27 11:05:32 -0400

    Co-authored-by: Gionas361 <Gionas361@users.noreply.github.com> Co-authored-by: Sweety <SMCastr@users.noreply.github.com>

--- main.h
+++ main.h
@@ -3,4 +3,12 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+typedef int (*format_func_ptr)(va_list);
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+
 #endif
